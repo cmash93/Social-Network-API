75c67a51f17d0cc73fd5523614a5cfba46684aa4
cmash93 <cmash93@gmail.com>
2022-12-08 18:14:38 -0600

    user model

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const { connect, connection } = require('mongoose');
 // After you create your Heroku application, visit https://dashboard.heroku.com/apps/ select the application name and add your Atlas connection string as a Config Var
 
 const connectionString =
-    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialDB';
 
 connect(connectionString, {
     useNewUrlParser: true,
